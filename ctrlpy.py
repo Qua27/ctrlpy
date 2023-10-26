61a7ed46a830d9d965b47c6898a98846aae6dfec
Qua27 <92877777+Qua27@users.noreply.github.com>
2023-10-26 13:01:30 +0300

    v0.9.5

--- ctrlpy.py
+++ ctrlpy.py
@@ -21,14 +21,14 @@ FunctionData = namedtuple('FunctionData', ['xlabel', 'ylabel', 'title'])
 class System:
     _s = sp.symbols('s')
     _t, _w = sp.symbols('t w', real=True)
-    _names = {'response': FunctionData('t', 'y', 'Response to Input Signal'),
-              'step_response': FunctionData('t', 'y', 'Step Response'),
-              'delta_response': FunctionData('t', 'y', 'Delta Response'),
-              're_freq': FunctionData('w', 'Re H(jw)', 'Real part of H(jw)'),
-              'im_freq': FunctionData('w', 'Im H(jw)', 'Imaginary part of H(jw)'),
-              'magnitude_freq': FunctionData('w', '|H(jw)|', 'Magnitude-Frequency Response'),
-              'phase_freq': FunctionData('w', 'arg(H(jw))', 'Phase-Frequency Response'),
-              'log_magnitude': FunctionData('w', 'dB', 'Logarithmic Magnitude-Frequency Response')
+    _names = {'response': FunctionData('t, s', 'y', 'Response to Input Signal'),
+              'step_response': FunctionData('t, s', 'y', 'Step Response'),
+              'delta_response': FunctionData('t, s', 'y', 'Delta Response'),
+              're_freq': FunctionData('w, rad/s', 'Re H(jw)', 'Real part of H(jw)'),
+              'im_freq': FunctionData('w, rad/s', 'Im H(jw)', 'Imaginary part of H(jw)'),
+              'magnitude_freq': FunctionData('w, rad/s', '|H(jw)|', 'Magnitude-Frequency Response'),
+              'phase_freq': FunctionData('w, rad/s', 'arg(H(jw)), rad', 'Phase-Frequency Response'),
+              'log_magnitude': FunctionData('w, rad/s', 'dB', 'Logarithmic Magnitude-Frequency Response')
               }
 
     def __init__(self, tf: str):
@@ -52,16 +52,25 @@ class System:
     def delta_response(self) -> sp.Expr:
         return self.response('DiracDelta(t)')
 
-    def plot(self, function: Callable[[], sp.Expr], plot_range: tuple[float, float] = (-10, 10),
-             func_to_compare_with: Callable[[float | np.ndarray], float | np.ndarray] = None,
-             title: str = None) -> None:
+    def plot(self, function: Callable[[], sp.Expr], plot_range: tuple[float, float] = (0, 10),
+             log_scale: bool | None = None,
+             func_to_compare_with: Callable[[float | np.ndarray], float | np.ndarray] | None = None,
+             title: str | None = None) -> None:
+        if log_scale is None and function.__name__ == 'log_magnitude':
+            log_scale = True
+        assert isinstance(function(), sp.Expr), 'We must have sympy.Expr returning functions'
         if not hasattr(self, function.__name__):
             raise AttributeError('Invalid function passed. You can only pass System class methods to plot')
         fdata = self._names.get(function.__name__)
         assert len(function().free_symbols) == 1, 'We cannot have more than one variable here'
-        l_func = sp.lambdify(fdata.xlabel, function(), 'numpy')
+        l_func = sp.lambdify(fdata.xlabel[0], function(), 'numpy')
         x = np.linspace(*plot_range, 1000)
         plt.plot(x, l_func(x), label='Response through transfer function')
+        if log_scale:
+            plt.xscale('log')
+
+            # TODO: implement proper yticks including -3 and -17 and other for other cases for log_magnitude
+
         if func_to_compare_with is not None:
             plt.plot(x, func_to_compare_with(x), label='Analytical response')
             plt.legend()
@@ -99,5 +108,17 @@ class System:
     def log_magnitude(self) -> sp.Expr:
         return 20 * sp.log(self.magnitude_freq()) / sp.log(10)
 
-    def __nyquist(self):
+    def __nyquist(self) -> tuple[
+        Callable[[float | np.ndarray], float | np.ndarray],
+        Callable[[float | np.ndarray], float | np.ndarray],
+    ]:
         return sp.lambdify(self._w, self.re_freq(), 'numpy'), sp.lambdify(self._w, self.im_freq(), 'numpy')
+
+    def plot_nyquist(self, freq_range: tuple[float, float] = (0, 20), title: str | None = None) -> None:
+        w = np.linspace(*freq_range, 10000)
+        plt.plot(self.__nyquist()[0](w), self.__nyquist()[1](w))
+        plt.title(title) if title is not None else plt.title('Nyquist')
+        plt.xlabel('Re H(jw)')
+        plt.ylabel('Im H(jw)')
+        plt.grid()
+        plt.show()
